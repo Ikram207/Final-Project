f8092ab5bc9fd0aef952f7beb72f45446310049d
ikram207 <ikrambdk55@gmail.com>
2025-08-03 10:33:06 +0100

    premier commit

---
+++ server/models/Task.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: String,
+  status: {
+    type: String,
+    enum: ['in progress', 'completed'],
+    default: 'in progress'
+  },
+  deadline: Date
+}, { timestamps: true });
+
+module.exports = mongoose.model('Task', taskSchema);
